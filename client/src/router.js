623a8ab7843144a271fbed309151aac00fee5002
Víctor Peña Romero <vic.pero@icloud.com>
2021-09-20 07:28:49 -0500

    client: remove navigation

--- client/src/router.js
+++ client/src/router.js
@@ -1,16 +1,13 @@
 import { Switch, Route } from 'react-router-dom'
-import Navbar from 'src/components/core/Navbar'
 import Home from 'src/views/Home'
 import Weight from 'src/views/Weight'
 
 const Router = () => (
-  <>
-    <Navbar />
-    <Switch>
-      <Route exact path='/weight' component={ Weight } />
-      <Route exact path='/' component={ Home } />
-    </Switch>
-  </>
+  <Switch>
+    <Route exact path='/weight' component={ Weight } />
+    <Route exact path='/' component={ Home } />
+  </Switch>
+
 )
 
 export default Router
